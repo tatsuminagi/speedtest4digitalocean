d75b344375c8d77c8fb886dcfb67ec067c44d57f
tatsuminagi <tatsuminagi@users.noreply.github.com>
2017-01-05 19:35:48 +0800

    box

--- AnalyseResult.py
+++ AnalyseResult.py
@@ -56,8 +56,19 @@ def ReadDownloadResults(filename):
     return downloadRes, np.array(hourList)
 
 if __name__ == '__main__':
-    pingRes = ReadPingResults('result_ping.txt')
-    downloadRes, hourList = ReadDownloadResults('result_download.txt')
+    pingRes = ReadPingResults('result_ping2.txt')
+    downloadRes, hourList = ReadDownloadResults('result_download2.txt')
+    
+    hourList_ = np.zeros((hourList.shape), dtype=np.int)
+    hourListInd = np.zeros((hourList.shape), dtype=np.int)
+    for i in range(2, hourList.shape[0]):
+        if abs(hourList[-i] - hourList[-i+1]) != 1 and hourList[-i] != 23:
+            hourList_[0:i-1] = hourList[-i+1:]
+            hourListInd = range(hourList.shape[0]-i+1, hourList.shape[0])
+            hourList_[i-1:] = hourList[:-i+1]
+            hourListInd += range(0, hourList.shape[0]-i+1)
+    hourListInd = np.array(hourListInd)
+    
     keys = ['nyc1', 'nyc2', 'nyc3', 'ams2', 'ams3', 'sfo1', 'sfo2', 
             'sgp1', 'lon1', 'fra1', 'tor1', 'blr1']
     
@@ -71,36 +82,49 @@ if __name__ == '__main__':
         print("\tspeed: {0:.3f} KB/s ({1:.3f})".format(speed, speedStd))
         print("\tping:  {0:.3f} ms   ({1:.3f})".format(pingTime, pingTimeStd))
         print("\tloss:  {0:.3f} %    ({1:.3f})".format(pingLoss, pingLossStd))
+        
+    nb_smooth1 = 200 * (np.argmax(hourList_)+1) / hourList_.shape[0]
+    x_smooth1 = np.linspace(hourList_[0], hourList_.max(), nb_smooth1)
+    x_smooth2 = np.linspace(hourList_.min(), hourList_[-1], 200-nb_smooth1)    
+    x_smooth = np.concatenate((x_smooth1, x_smooth2))
     
-    x_smooth = np.linspace(0, 23, 200)
+    x_smooth = np.linspace(0, hourList_.shape[0], 200)
     
     plt.figure(0)    
     for key in keys:
-        y_smooth = spline(np.arange(24), downloadRes[key], x_smooth)
+        y_smooth = spline(np.arange(hourList_.shape[0]), downloadRes[key][hourListInd], x_smooth)
         plt.plot(x_smooth, y_smooth, linewidth=2, label='{0}'.format(key))
     
     plt.xlabel("Hour in one day")  
     plt.ylabel("Download Speed (KB/s)")      
     plt.legend()
     
-    
     plt.figure(1)    
     for key in keys:
-        y_smooth = spline(np.arange(24), pingRes[key][:, 0], x_smooth)
-        plt.plot(x_smooth, y_smooth, linewidth=2, label='{0}'.format(key))
+        #y_smooth = spline(np.arange(hourList_.shape[0]), downloadRes[key][hourList_], x_smooth)
+        plt.plot(hourListInd, downloadRes[key][hourListInd], linewidth=2, label='{0}'.format(key))
     
     plt.xlabel("Hour in one day")  
-    plt.ylabel("Avg ping respond time (ms)")      
-    plt.legend()
-    
-    plt.figure(2)    
-    for key in keys:
-        y_smooth = spline(np.arange(24), pingRes[key][:, 1], x_smooth)
-        plt.plot(x_smooth, y_smooth, linewidth=2, label='{0}'.format(key))
-    
-    plt.xlabel("Hour in one day")  
-    plt.ylabel("Pack loss percentage")      
+    plt.ylabel("Download Speed (KB/s)")      
     plt.legend()
     
     plt.show()
+    
+    #plt.figure(1)    
+    #for key in keys:
+        #y_smooth = spline(np.arange(24), pingRes[key][:, 0], x_smooth)
+        #plt.plot(x_smooth, y_smooth, linewidth=2, label='{0}'.format(key))
+    
+    #plt.xlabel("Hour in one day")  
+    #plt.ylabel("Avg ping respond time (ms)")      
+    #plt.legend()
+    
+    #plt.figure(2)    
+    #for key in keys:
+        #y_smooth = spline(np.arange(24), pingRes[key][:, 1], x_smooth)
+        #plt.plot(x_smooth, y_smooth, linewidth=2, label='{0}'.format(key))
+    
+    #plt.xlabel("Hour in one day")  
+    #plt.ylabel("Pack loss percentage")      
+    #plt.legend()
     
